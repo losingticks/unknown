337eadafe0f6e681d24fee93afad945ad64e647f
losing ticks <51202585+losingticks@users.noreply.github.com>
2022-07-04 15:27:41 -0500

    drawing something

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,7 +1,11 @@
 import java.awt.BorderLayout;
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 import javax.swing.JFrame;
 
 public class Game extends Canvas implements Runnable
@@ -10,20 +14,57 @@ public class Game extends Canvas implements Runnable
     public static final int WIDTH = 352 * 2;
     public static final int HEIGHT = 224 * 2;
 
+    private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+    private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+
+    private boolean running = false;
+    private int tickCount;
+
     public void start()
     {
-
+        running = true;
+        new Thread(this).start();
     }
 
     public void stop()
     {
+        running = false;
+    }
 
+    public void tick()
+    {
+        tickCount++;
+    }
+
+    public void render()
+    {
+        BufferStrategy bs = getBufferStrategy();
+
+        if (bs == null)
+        {
+            createBufferStrategy(3);
+            return;
+        }
+
+        for (int i = 0; i < pixels.length; i++)
+        {
+            pixels[i] = i + tickCount;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+        g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+        g.dispose();
+        bs.show();
     }
 
     @Override
     public void run()
     {
-
+        while (running)
+        {
+            tick();
+            render();
+        }
     }
 
     public static void main(String[] args)
@@ -41,5 +82,7 @@ public class Game extends Canvas implements Runnable
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+
+        game.start();
     }
 }
